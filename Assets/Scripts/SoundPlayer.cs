c1bd34ad665109a51641d7670802040b046331c6
Pinbrawl <sans-andertale@mail.ru>
2025-05-08 13:41:23 +0400

    Refactoring 08.05.25 1

--- Assets/Scripts/SoundPlayer.cs
+++ Assets/Scripts/SoundPlayer.cs
@@ -1,29 +1,44 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundPlayer : MonoBehaviour
 {
-    [SerializeField] private AudioSource _firstSound;
-    [SerializeField] private AudioSource _secondSound;
-    [SerializeField] private AudioSource _thirdSound;
+    [SerializeField] private Button _firstButton;
+    [SerializeField] private Button _secondButton;
+    [SerializeField] private Button _thirdButton;
+    [SerializeField] private AudioClip _firstSound;
+    [SerializeField] private AudioClip _secondSound;
+    [SerializeField] private AudioClip _thirdSound;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        _firstButton.onClick.AddListener(PlayFirst);
+        _secondButton.onClick.AddListener(PlaySecond);
+        _thirdButton.onClick.AddListener(PlayThird);
+    }
 
     public void PlayFirst()
     {
-        _secondSound.Stop();
-        _thirdSound.Stop();
-        _firstSound.Play();
+        _audioSource.Stop();
+        _audioSource.PlayOneShot(_firstSound);
     }
 
     public void PlaySecond()
     {
-        _firstSound.Stop();
-        _thirdSound.Stop();
-        _secondSound.Play();
+        _audioSource.Stop();
+        _audioSource.PlayOneShot(_secondSound);
     }
 
     public void PlayThird()
     {
-        _firstSound.Stop();
-        _secondSound.Stop();
-        _thirdSound.Play();
+        _audioSource.Stop();
+        _audioSource.PlayOneShot(_thirdSound);
     }
 }
